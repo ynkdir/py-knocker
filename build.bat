ed97f22fa448ffb8b7b0f7b1fe572cca99d63e24
Yukihiro Nakadaira <yukihiro.nakadaira@gmail.com>
2023-01-02 03:47:07 +0900

    no comment

--- build.bat
+++ build.bat
@@ -3,3 +3,8 @@ cl /nologo /W4 /O2 /MD /Femainw_stub.exe main.c /link /SUBSYSTEM:WINDOWS /ENTRY:
 py -m zipapp bootstrap
 copy /b main_stub.exe + bootstrap.pyz main.exe
 copy /b mainw_stub.exe + bootstrap.pyz mainw.exe
+
+rem example using resource
+rc /nologo main.rc
+cl /nologo /W4 /O2 /MD /Femain_with_resource.exe main.c main.res
+cl /nologo /W4 /O2 /MD /Femainw_with_resource.exe main.c main.res /link /SUBSYSTEM:WINDOWS /ENTRY:wmainCRTStartup
