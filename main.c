d273a683a4f50fa1ff27eb9de1a9d6259973dc5b
Yukihiro Nakadaira <yukihiro.nakadaira@gmail.com>
2025-01-13 02:00:52 +0900

    no comment

--- main.c
+++ main.c
@@ -1,33 +1,42 @@
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
-#include <shlwapi.h>
 #include <pathcch.h>
 #include <stdlib.h>
 #include <string.h>
+#include <wchar.h>
 #pragma comment(lib, "onecore.lib")
 
 #ifndef PYTHON_DLL_PATH
-# define PYTHON_DLL_PATH L"..\\python\\python3.dll"
+# define PYTHON_DLL_PATH L"python\\python3.dll"
 #endif
 
 typedef int (*PY_MAIN)(int, wchar_t**);
 
-wchar_t *GetModuleName() {
-    wchar_t path[MAX_PATH];
-    if (GetModuleFileNameW(NULL, path, MAX_PATH) == 0)
+wchar_t *wcsndup(const wchar_t *src, size_t size) {
+    wchar_t *dst = malloc(sizeof(wchar_t) * (size + 1));
+    if (!dst)
+        return NULL;
+    wmemmove(dst, src, size);
+    dst[size] = L'\0';
+    return dst;
+}
+
+// only for .exe file
+wchar_t *path_stem(wchar_t *path) {
+    wchar_t *sep = NULL;
+    wchar_t *ext = NULL;
+
+    for (wchar_t *p = path; *p; ++p) {
+        if (*p == L'/' || *p == L'\\')
+            sep = p;
+        else if (*p == L'.')
+            ext = p;
+    }
+
+    if (sep == NULL || ext == NULL || ext < sep)
         return NULL;
 
-    wchar_t *filename = PathFindFileNameW(path);
-    if (filename == path)
-        return NULL;
-
-    wchar_t *ext;
-    if (FAILED(PathCchFindExtension(path, MAX_PATH, &ext)))
-        return NULL;
-
-    *ext = 0;
-
-    return _wcsdup(filename);
+    return wcsndup(sep + 1, ext - sep - 1);
 }
 
 int wmain(int argc, wchar_t **argv) {
@@ -36,13 +45,18 @@ int wmain(int argc, wchar_t **argv) {
         return EXIT_FAILURE;
 
     wchar_t path[MAX_PATH] = PYTHON_DLL_PATH;
-    // Optimizer may drop this block for absolute PYTHON_DLL_PATH.
-    if (PYTHON_DLL_PATH[0] == L'.') {
-        if (GetModuleFileNameW(NULL, path, MAX_PATH) == 0)
-            return EXIT_FAILURE;
-        if (FAILED(PathCchCombine(path, MAX_PATH, path, PYTHON_DLL_PATH)))
-            return EXIT_FAILURE;
-    }
+    if (GetModuleFileNameW(NULL, path, MAX_PATH) == 0)
+        return EXIT_FAILURE;
+
+    wchar_t *module = path_stem(path);
+    if (!module)
+        return EXIT_FAILURE;
+
+    if (FAILED(PathCchRemoveFileSpec(path, MAX_PATH)))
+        return EXIT_FAILURE;
+
+    if (FAILED(PathCchCombine(path, MAX_PATH, path, PYTHON_DLL_PATH)))
+        return EXIT_FAILURE;
 
     HMODULE pydll = LoadLibraryExW(path, NULL, LOAD_LIBRARY_SEARCH_DEFAULT_DIRS|LOAD_LIBRARY_SEARCH_DLL_LOAD_DIR);
     if (!pydll)
@@ -52,22 +66,18 @@ int wmain(int argc, wchar_t **argv) {
     if (!Py_Main)
         return EXIT_FAILURE;
 
-    wchar_t *module = GetModuleName();
-    if (!module)
-        return EXIT_FAILURE;
-
     // Workaround for multiprocessing.
     // FIXME: or myargv[0] = "path\\to\\python.exe"?
     for (int i = 1; i < argc; ++i)
         if (wcscmp(argv[i], L"--multiprocessing-fork") == 0)
             return Py_Main(argc, argv);
 
-    wchar_t **myargv = _alloca((argc + 3) * sizeof(wchar_t*));
+    wchar_t **myargv = _alloca((argc + 4) * sizeof(wchar_t*));
     myargv[0] = argv[0];
     myargv[1] = L"-I";  // isolated mode
     myargv[2] = L"-m";
     myargv[3] = module;
-    memcpy(myargv + 4, argv + 1, (argc - 1) * sizeof(wchar_t *));
+    memmove(myargv + 4, argv + 1, (argc - 1) * sizeof(wchar_t *));
     return Py_Main(argc + 3, myargv);
 }
 
